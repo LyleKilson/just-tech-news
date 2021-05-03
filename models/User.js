021929d98fa890c2abfd698d61c63dd6c5cdd7cb
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-03 15:01:23 -0600

    added hooks and instance method

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -54,7 +59,10 @@ User.init(
       },
       // set up beforeUpdate lifecycle "hook" functionality
       async beforeUpdate(updateUserData) {
-        updatedUserData.password = await bcrypt.hash(updateUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(
+          updateUserData.password,
+          10
+        );
         return updatedUserData;
       },
     },
