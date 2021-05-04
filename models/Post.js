c62aef62365e1c599c256f4e04ef1188b650c717
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-04 12:47:39 -0600

    updated snapshot

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,48 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id,
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id,
+        },
+        attributes: [
+          "id",
+          "post_url",
+          "title",
+          "created_at",
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count",
+          ],
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: [
+              "id",
+              "comment_text",
+              "post_id",
+              "user_id",
+              "created_at",
+            ],
+            include: {
+              model: models.User,
+              attributes: ["username"],
+            },
+          },
+        ],
+      });
+    });
+  }
+}
 
 // create fields/columns for Post model
 Post.init(
@@ -21,7 +61,7 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isUrl: true,
+        isURL: true,
       },
     },
     user_id: {
@@ -39,4 +79,5 @@ Post.init(
     modelName: "post",
   }
 );
+
 module.exports = Post;
